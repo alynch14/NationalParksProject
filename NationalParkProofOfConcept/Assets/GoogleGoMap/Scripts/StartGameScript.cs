d8bdd26d519500321f397ba95714496d6d908400
Adam Lynch <alynch14@students.towson.edu>
2018-03-05 19:12:22 -0500

    GameDevStart
    This branch is going to be deprecated when I move all of our code to the new repo in our group.

---
+++ NationalParkProofOfConcept/Assets/GoogleGoMap/Scripts/StartGameScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour {
+
+    int sceneNum;
+    Button button;
+    SceneManager loader;
+    // Use this for initialization
+    void Start () {
+        sceneNum = 0;
+        button = button.GetComponent<Button>();
+        loader.GetType(Scene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        button.onClick.AddListener(ChangeScene);
+	}
+
+    public void ChangeScene()
+    {
+        loader
+    }
+
+   
+}
---
+++ NationalParkProofOfConcept/Assets/GoogleGoMap/Scripts/StartMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMap : MonoBehaviour {
+    bool start;
+    GoogleStaticMap goog;
+	// Use this for initialization
+	void Start () {
+        onEnterScene(start);
+	}
+	
+    void onEnterScene(bool flag)
+    {
+        if (flag)
+        {
+            goog = new GoogleStaticMap();
+            goog.initialize();
+            goog.DrawMap();
+        }
+    }
+}
